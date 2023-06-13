561b01ff20e6dc46495d1895c0ee4ef6d869410c
ProgramerNila <programernila@gmail.com>
2023-06-13 17:52:09 +0600

    add r

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 let _ = express.Router();
 let registation= require("./auth.js")
+let category= require("./category.js")
 _.use("/auth", registation);
+_.use(category);
 module.exports = _;
